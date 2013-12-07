1c8e55c300535e3ad5d159a6fd2eadc8b1dbc72c
Alexander Markov <apsheronets@gmail.com>
2013-12-07 22:14:30 +0400

    fixed release script

--- devel/release.bash
+++ devel/release.bash
@@ -23,7 +23,7 @@ function release_sources {
 }
 
 function release_build {
-  cd $chroot_dist_dir || exit 1
+  cd $chroot_dist_dir/$name || exit 1
   make -s clean || exit 1
 
   make -s -j4 ${name} || exit 1
@@ -49,12 +49,12 @@ case "$1" in
   sources) release_sources;;
   build-root) su -c "$0 build" $user;;
   build) release_build;;
-  *) $0 sources &&
-    rm -rf $chroot_dir/$chroot_dist_dir/$name &&
-    cd $chroot_dist_dir &&
-    git clone $basedir &&
-    cd $basedir &&
-    chmod +x $chroot_dir/$chroot_dist_dir/$name/devel/release.bash &&
+  *) $0 sources || exit 1
+    rm -rf $chroot_dir/$chroot_dist_dir/$name || exit 1
+    cd $chroot_dir/$chroot_dist_dir || exit 1
+    git clone $basedir || exit 1
+    cd $basedir || exit 1
+    chmod +x $chroot_dir/$chroot_dist_dir/$name/devel/release.bash || exit 1
     sudo chroot $chroot_dir $chroot_dist_dir/$name/devel/release.bash build-root;
     mkdir -p $tar_dst/builded/;
     cp $chroot_dir/$chroot_dist_dir/$name/$byte.tar.gz $tar_dst/builded/;
